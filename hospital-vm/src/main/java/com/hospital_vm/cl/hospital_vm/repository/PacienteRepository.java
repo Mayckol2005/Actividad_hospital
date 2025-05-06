6c82d7756eae6c4cfcbcd26511c3dd27d99f6fb6
Mayckol2005 <may.mardones@duocuc.cl>
2025-05-06 12:28:12 -0400

    guardadov2

---
+++ hospital-vm/src/main/java/com/hospital_vm/cl/hospital_vm/repository/PacienteRepository.java
@@ -0,0 +1,12 @@
+package com.hospital_vm.cl.hospital_vm.repository;
+
+import com.hospital_vm.cl.hospital_vm.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    // Encuentra pacientes por apellidos
+    List<Paciente> findByApellidos(String apellidos);
+}
